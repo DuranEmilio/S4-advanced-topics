74b1a4867dd817170179796ed1bda4d9d1010f5c
emilioDuran <durantelleze@gmail.com>
2026-02-05 11:56:54 -0600

    add components in the JPanel

--- src/U1/Start/Window.java
+++ src/U1/Start/Window.java
@@ -1,14 +1,16 @@
 package U1.Start;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
   private JLabel lbF,lbC; //TAGS
   private JTextField txtF, txtC; //TEXT FIELDS
-  private JButton cleanBtn, convertBtn;
+  private JButton cleanBtn, convertBtn; //BUTTONS
 
   public Window() {
     windowSettings();
+    initComponent();
   }
 
   private void windowSettings() {
@@ -16,9 +18,24 @@ public class Window extends JFrame {
     this.setTitle("Conversor de temperaturas");
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     this.setLocationRelativeTo(null);
+    this.setLayout(new GridLayout(3,2,5,5));
   }
 
   private void initComponent() {
+    lbC = new JLabel("Grados celsius");
+    lbF = new JLabel("Grados fahrenheit");
 
+    txtC = new JTextField();
+    txtF = new JTextField();
+
+    convertBtn = new JButton("Convertir");
+    cleanBtn = new JButton("Limpiar");
+
+    this.add(lbF);
+    this.add(txtF);
+    this.add(lbC);
+    this.add(txtC);
+    this.add(convertBtn);
+    this.add(cleanBtn);
   }
 }
