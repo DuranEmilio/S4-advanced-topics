38825d8b35a6a7752698ae70e0d56618594fbc39
emilioDuran <durantelleze@gmail.com>
2026-02-09 12:01:25 -0600

    convert fields and exception manage

--- src/U1/Start/Window.java
+++ src/U1/Start/Window.java
@@ -2,9 +2,12 @@ package U1.Start;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Window extends JFrame {
-  private JLabel lbF,lbC; //TAGS
+public class Window extends JFrame implements ActionListener {
+
+  private JLabel lbF, lbC; //TAGS
   private JTextField txtF, txtC; //TEXT FIELDS
   private JButton cleanBtn, convertBtn; //BUTTONS
 
@@ -18,7 +21,7 @@ public class Window extends JFrame {
     this.setTitle("Conversor de temperaturas");
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     this.setLocationRelativeTo(null);
-    this.setLayout(new GridLayout(3,2,5,5));
+    this.setLayout(new GridLayout(3, 2, 5, 5));
   }
 
   private void initComponent() {
@@ -29,7 +32,9 @@ public class Window extends JFrame {
     txtF = new JTextField();
 
     convertBtn = new JButton("Convertir");
+    convertBtn.addActionListener(this);
     cleanBtn = new JButton("Limpiar");
+    cleanBtn.addActionListener(this);
 
     this.add(lbF);
     this.add(txtF);
@@ -37,5 +42,27 @@ public class Window extends JFrame {
     this.add(txtC);
     this.add(convertBtn);
     this.add(cleanBtn);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == convertBtn) {
+      double f = 0, c = 0;
+      try {
+        if (!txtC.getText().isEmpty()) {
+          f = Double.parseDouble(txtC.getText());
+          c = (f - 32) * 5/9;
+          txtF.setText(String.format("%.1f", c));
+        } else if (!txtF.getText().isEmpty()) {
+          c = Double.parseDouble(txtF.getText());
+        }
+      } catch (NumberFormatException ex) {
+
+      }
+    } else if (e.getSource() == cleanBtn) {
+      txtC.setText("");
+      txtF.setText("");
+      txtF.requestFocus();
+    }
   }
 }
